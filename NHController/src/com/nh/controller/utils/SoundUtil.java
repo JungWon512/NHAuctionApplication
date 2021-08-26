4aedc6b0a471f8a6c47ffe1ef4493832be69c230
hmju <hmju@eb7ece3d-c1b3-5749-b099-a045586364b9>
2021-08-26 02:19:41 +0000

    - Google Cloud TTS 기능 추가 - google_tts_service.json 추가 - TTS Jar 추가 - Audio Player Jar 추가

--- NHController/src/com/nh/controller/utils/SoundUtil.java
+++ NHController/src/com/nh/controller/utils/SoundUtil.java
@@ -1,80 +1,132 @@
 package com.nh.controller.utils;
 
+import com.google.cloud.texttospeech.v1.*;
+import com.nh.share.api.ActionRuler;
+import javazoom.jl.player.Player;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
 import java.lang.invoke.MethodHandles;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import com.nh.share.common.models.Bidding;
-
 /**
  * 음성 실행 클래스
- * @author jhlee
  *
+ * @author jhlee
  */
 public class SoundUtil {
-	
-	private final Logger mLogger = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
-	
-	private List<String> mSoundDataList = new ArrayList<String>();
 
-	private int mCurrentIndex = -1;
+    private final Logger mLogger = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 
-	public void setSoundDataList(List<String> dataList) {
-		mSoundDataList.clear();
-		mSoundDataList.addAll(dataList);
-	}
-	
-	/**
-	 * 음성 시작
-	 * 
-	 * @param index
-	 */
-	public void playSound(int index) {
-		
-		if(index < 0) {
-			mLogger.debug("SOUND INDEX IS NULL !!");
-			return;
-		}
-		
-		if(CommonUtils.getInstance().isListEmpty(mSoundDataList)) {
-			mLogger.debug("SOUND IS NULL !!");
-			return;
-		}
+    private List<String> mSoundDataList = new ArrayList<String>();
 
-		String strSound = mSoundDataList.get(index);
+    private int mCurrentIndex = -1;
+    private final VoiceSelectionParams mVoice;
+    private final AudioConfig mAudioConfig;
+    private TextToSpeechClient mClient;
 
-		if (strSound != null && !strSound.isEmpty() && !strSound.isBlank()) {
-			mCurrentIndex = index;
-			mLogger.debug("PLAY SOUND : " + strSound);
-		} else {
-			mLogger.debug("SOUND IS NULL !!");
-		}
-	}
+    public SoundUtil() {
+        mVoice = VoiceSelectionParams.newBuilder()
+                .setLanguageCode("ko-KR")
+                .setSsmlGender(SsmlVoiceGender.FEMALE)
+                .build();
 
-	/**
-	 * 음성 정지
-	 * 
-	 * @param index
-	 */
-	public void stopSound(int index) {
-		mLogger.debug("stopSound index : " + index);
-	}
-	
-	
-	/**
-	 * 음성 정지
-	 * 
-	 * @param index
-	 */
-	public void stopSound() {
-		mLogger.debug("stopSound");
-	}
-	
-	
-	public int getCurrentIndex() {
-		return mCurrentIndex;
-	}
+        mAudioConfig = AudioConfig.newBuilder()
+                .setAudioEncoding(AudioEncoding.MP3)
+                .build();
+        try {
+            /**
+             * Google Cloud Service 사용하려면 Environment variables에 환경변수 처리 해야함
+             * ${KEY}${VALUE}
+             * GOOGLE_APPLICATION_CREDENTIALS=C:\workStudio\AuctionApplications\NHController\google_tts_service.json
+             */
+            mClient = TextToSpeechClient.create();
+        } catch (Exception ex) {
+            mLogger.error("TextToSpeechClient Error " + ex.getMessage());
+        }
+    }
+
+    public void setSoundDataList(List<String> dataList) {
+        mSoundDataList.clear();
+        mSoundDataList.addAll(dataList);
+    }
+
+    /**
+     * 음성 시작
+     *
+     * @param index
+     */
+    public void playSound(int index) {
+
+        if (index < 0) {
+            mLogger.debug("SOUND INDEX IS NULL !!");
+            return;
+        }
+
+        if (CommonUtils.getInstance().isListEmpty(mSoundDataList)) {
+            mLogger.debug("SOUND IS NULL !!");
+            return;
+        }
+
+        String strSound = mSoundDataList.get(index);
+
+        if (strSound != null && !strSound.isEmpty() && !strSound.isBlank()) {
+            mCurrentIndex = index;
+            ActionRuler.getInstance().addRunnable(new TextToSpeechRunnable(strSound));
+            ActionRuler.getInstance().runNext();
+            mLogger.debug("PLAY SOUND : " + strSound);
+        } else {
+            mLogger.debug("SOUND IS NULL !!");
+        }
+    }
+
+    /**
+     * 음성 정지
+     *
+     * @param index
+     */
+    public void stopSound(int index) {
+        mLogger.debug("stopSound index : " + index);
+    }
+
+
+    /**
+     * 음성 정지
+     */
+    public void stopSound() {
+        mLogger.debug("stopSound");
+    }
+
+
+    public int getCurrentIndex() {
+        return mCurrentIndex;
+    }
+
+    /**
+     * Google Cloud TTS Runnable Class
+     */
+    class TextToSpeechRunnable implements Runnable {
+
+        private final String msg;
+
+        TextToSpeechRunnable(String msg) {
+            this.msg = msg;
+        }
+
+        @Override
+        public void run() {
+            try {
+                SynthesisInput input = SynthesisInput.newBuilder().setText(msg).build();
+                SynthesizeSpeechResponse response = mClient.synthesizeSpeech(input, mVoice, mAudioConfig);
+                InputStream stream = new ByteArrayInputStream(response.getAudioContent().toByteArray());
+                Player mp3Play = new Player(stream);
+                mp3Play.play();
+            } catch (Exception ex) {
+                mLogger.error("textToSpeech Error " + ex.getMessage());
+            }
+        }
+    }
 }
