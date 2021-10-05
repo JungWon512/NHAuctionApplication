7aa7ccf619f24f721689efd38c708b00e3b297f3
jspark <jspark@eb7ece3d-c1b3-5749-b099-a045586364b9>
2021-10-05 06:58:25 +0000

    RetryTargetInfo WebSocket 로직 수정 적용

--- NHAuctionServer/src/main/java/com/nh/auctionserver/socketio/SocketIOHandler.java
+++ NHAuctionServer/src/main/java/com/nh/auctionserver/socketio/SocketIOHandler.java
@@ -902,6 +902,10 @@ public class SocketIOHandler {
 							client.sendEvent("AuctionStatus",
 									mAuctioneer.getAuctionState(connectionInfo.getAuctionHouseCode()).getAuctionStatus()
 											.getEncodedMessage());
+							
+							if (mAuctioneer.getAuctionState(connectionInfo.getAuctionHouseCode()).getRetryTargetInfo() != null) {
+								client.sendEvent("RetryTargetInfo", mAuctioneer.getAuctionState(responseConnectionInfo.getAuctionHouseCode()).getRetryTargetInfo().getEncodedMessage());
+							}
 						}
 					}
 				} else {
