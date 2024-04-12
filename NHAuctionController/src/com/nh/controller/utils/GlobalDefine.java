18815162f762c1ee97ed2196d6a7f2ef82580b5a
kylinho <kylinho21@gmail.com>
2024-04-12 16:55:19 +0900

    fxml 21 -> 16 수정,  프로그램버전 1.3.2.1

--- NHAuctionController/src/com/nh/controller/utils/GlobalDefine.java
+++ NHAuctionController/src/com/nh/controller/utils/GlobalDefine.java
@@ -17,8 +17,8 @@ public class GlobalDefine {
 
 		//public static final String RELEASE_VERION = "1.3.1"; // application 버전		
 		//public static final String RELEASE_DATE = "2023-03-16"; // 업데이트 날짜 
-		public static final String RELEASE_VERION = "1.3.2"; // application 버전		
-		public static final String RELEASE_DATE = "2024-02-27"; // 업데이트 날짜 
+		public static final String RELEASE_VERION = "1.3.2.1"; // application 버전		
+		public static final String RELEASE_DATE = "2024-04-12"; // 업데이트 날짜 
 
 	}
 	
